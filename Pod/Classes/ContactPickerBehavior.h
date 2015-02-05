09d1c53fe68e7f437a304b3572b0bcddcb0c1c97
Digitalforms <bieleckicezary@gmail.com>
2015-02-05 02:17:23 +0100

    Added behavior for contact.

---
+++ Pod/Classes/ContactPickerBehavior.h
@@ -0,0 +1,13 @@
+//
+//  ContactPickerBehavior.h
+//  Pods
+//
+//  Created by DigitalFormsMacbook on 04.02.2015.
+//
+//
+
+#import "Behavior.h"
+
+@interface ContactPickerBehavior : Behavior
+
+@end
