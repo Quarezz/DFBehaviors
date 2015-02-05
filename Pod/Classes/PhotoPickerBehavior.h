322a1eaf88a6b271e25453150dae47ff7238ef20
Digitalforms <bieleckicezary@gmail.com>
2015-02-05 02:14:49 +0100

    Added photo picker behavior.

---
+++ Pod/Classes/PhotoPickerBehavior.h
@@ -0,0 +1,13 @@
+//
+//  PhotoPickerBehavior.h
+//  Pods
+//
+//  Created by DigitalFormsMacbook on 04.02.2015.
+//
+//
+
+#import "Behavior.h"
+
+@interface PhotoPickerBehavior : Behavior
+
+@end
---
+++ Pod/Classes/PhotoPickerBehavior.m
@@ -0,0 +1,13 @@
+//
+//  PhotoPickerBehavior.m
+//  Pods
+//
+//  Created by DigitalFormsMacbook on 04.02.2015.
+//
+//
+
+#import "PhotoPickerBehavior.h"
+
+@implementation PhotoPickerBehavior
+
+@end
