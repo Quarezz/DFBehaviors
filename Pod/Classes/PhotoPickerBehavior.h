1d66d601f04473f996193bc641b875763d50a997
Digitalforms <bieleckicezary@gmail.com>
2015-02-05 02:17:41 +0100

    Added entire photo picker behavior implementation.

--- Pod/Classes/PhotoPickerBehavior.h
+++ Pod/Classes/PhotoPickerBehavior.h
@@ -8,6 +8,18 @@
 
 #import "Behavior.h"
 
-@interface PhotoPickerBehavior : Behavior
+typedef NS_ENUM(NSUInteger, DFImagePickerBehaviourSourceType) {
+    DFImagePickerBehaviourSourceTypeBoth = 0,
+    DFImagePickerBehaviourSourceTypeCamera = 1,
+    DFImagePickerBehaviourSourceTypeLibrary = 2,
+};
+
+@interface PhotoPickerBehavior : Behavior <UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIActionSheetDelegate>
+
+@property(nonatomic, assign) IBInspectable NSInteger sourceType;
+
+@property(nonatomic, weak) IBOutlet UIImageView *targetImageView;
+
+- (IBAction)pickImageFromButton:(UIButton *)sender;
 
 @end
--- Pod/Classes/PhotoPickerBehavior.m
+++ Pod/Classes/PhotoPickerBehavior.m
@@ -7,7 +7,61 @@
 //
 
 #import "PhotoPickerBehavior.h"
+#import <UIActionSheet+BlocksKit.h>
 
 @implementation PhotoPickerBehavior
+
+- (IBAction)pickImageFromButton:(UIButton *)sender
+{
+    UIActionSheet *actionSheet = [UIActionSheet bk_actionSheetWithTitle:nil];
+    
+    if (self.targetImageView.image) {
+        [actionSheet bk_setDestructiveButtonWithTitle:NSLocalizedString(@"Delete photo", nil) handler:^{
+            self.targetImageView.image = nil;
+        }];
+    }
+    
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera] && (self.sourceType == DFImagePickerBehaviourSourceTypeBoth || self.sourceType == DFImagePickerBehaviourSourceTypeCamera)) {
+        [actionSheet bk_addButtonWithTitle:NSLocalizedString(@"Take Photo", nil) handler:^{
+            [self showPickerWithSourceType:UIImagePickerControllerSourceTypeCamera];
+        }];
+    }
+    
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary] && (self.sourceType == DFImagePickerBehaviourSourceTypeBoth || self.sourceType == DFImagePickerBehaviourSourceTypeLibrary)) {
+        [actionSheet bk_addButtonWithTitle:NSLocalizedString(@"Choose Existing", nil) handler:^{
+            [self showPickerWithSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+        }];
+    }
+    
+    
+    [actionSheet bk_setCancelButtonWithTitle:NSLocalizedString(@"Cancel", nil) handler:nil];
+    [actionSheet showFromRect:sender.frame inView:sender.superview animated:YES];
+}
+
+- (void)showPickerWithSourceType:(UIImagePickerControllerSourceType)sourceType
+{
+    UIImagePickerController *controller = [UIImagePickerController new];
+    controller.modalPresentationStyle = UIModalPresentationCurrentContext;
+    controller.sourceType = sourceType;
+    controller.delegate = self;
+    
+    [self.owner presentViewController:controller animated:YES completion:nil];
+}
+
+#pragma mark - UIImagePickerDelegate
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage *image = [info valueForKey:UIImagePickerControllerOriginalImage];
+    self.targetImageView.image = image;
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
+    [self sendActionsForControlEvents:UIControlEventValueChanged];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
