c5bef698e67095da0a44871e34b708948a923743
Digitalforms <bieleckicezary@gmail.com>
2015-05-20 22:33:45 +0200

    Sound player behavior implementation added.

--- Pod/Classes/DFSoundPlayerBehavior.m
+++ Pod/Classes/DFSoundPlayerBehavior.m
@@ -10,6 +10,29 @@
 
 @implementation DFSoundPlayerBehavior
 
+- (void)playSound:(UIButton *)button
+{
+    
+}
 
+- (void)pauseSound:(UIButton *)button
+{
+    
+}
+
+- (void)stopSound:(UIButton *)button
+{
+    
+}
+
+- (void)volumeValueChanged:(UISlider *)slider
+{
+    
+}
+
+- (void)progressValueChanged:(UISlider *)slider
+{
+    
+}
 
 @end
